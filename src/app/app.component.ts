6a4a2f2ed3250b9ae4cc4d9229cfa7406e1c8fca
Mafoa <steve_mafoa@outlook.com>
2024-05-13 02:17:46 +0200

    Création d'une Interface pour mes données

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface DataCard {
+  bank: string,
+  numcard: string
+  expiry: string,
+  firstname: string,
+  lastname: string
+
+  
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +19,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'credit card';
+
+  card:  DataCard = {
+    bank: "Caisse d'épargne",
+    numcard: "5600 7890 1200 0340",
+    expiry: "12/28",
+    firstname: "Mafoa",
+    lastname: "Steve"
+
+  }
 }
