3fd0d3420ba416f48e25540190547c8984b7525f
Mafoa <steve_mafoa@outlook.com>
2024-05-14 16:28:32 +0200

    suppression de la methode displayNone sur le <p> pNone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 interface DataCard {
   bank: string,
-  numcard: string
+  numcard: string[]
   expiry: string,
   firstname: string,
   lastname: string
@@ -20,12 +20,26 @@ interface DataCard {
 })
 export class AppComponent {
 
+
+  pNone: 'visible' | 'hidden' = 'visible'
+
   card:  DataCard = {
     bank: "Caisse d'épargne",
-    numcard: "5600 7890 1200 0340",
+    numcard: ['5600', '7890', '1200', '0340'],
     expiry: "12/28",
-    firstname: "Mafoa",
-    lastname: "Steve"
-
+    firstname: "John",
+    lastname: "Doe"
   }
+
+  methodRandom(): number {
+    return Math.floor(Math.random() * 900) + 100
+  }
+  displayNone() {
+    if (this.pNone === 'visible') {
+      this.pNone = 'hidden'
+    } else {
+      this.pNone = 'visible'
+    }
+  } 
+  
 }
